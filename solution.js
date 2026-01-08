1421f76f7c3b4d5264aae9c1589fa5ad5b867a58
mbmugdho <mdmontasirbillah5@gmail.com>
2026-01-08 13:14:35 +0600

    added all solutions

--- solution.js
+++ solution.js
@@ -80,7 +80,8 @@ function findMaximumNumber(numberArray) {
   }
   return maximumNumber
 }
-console.log(findMaximumNumber([5, 1, 9, 3]))
+
+// console.log(findMaximumNumber([5, 1, 9, 3]))
 
 // Problem 5: Remove Duplicates from an Array
 // Write a function that removes all duplicate numbers from an array.
@@ -105,4 +106,106 @@ function removeDuplicatesNumbers(numberArray) {
   return newArray
 }
 
-console.log(removeDuplicatesNumbers([1, 2, 2, 3, 4, 4]))
+// console.log(removeDuplicatesNumbers([1, 2, 2, 3, 4, 4]))
+
+// Problem 6: Sum of All Numbers in an Array
+// Write a function that returns the sum of all numbers in an array.
+// Example:
+// Input: [1, 2, 3, 4]
+// Output: 10
+
+// Solution06:
+
+function sumOfAllNumbers(numberArray) {
+  let sum = 0
+  for (let index = 0; index < numberArray.length; index++) {
+    sum = sum + numberArray[index]
+  }
+  return sum
+}
+// console.log(sumOfAllNumbers([1, 2, 3, 4]));
+
+// Problem 7: Find Even Numbers in an Array
+// Write a function that returns all even numbers from a given array.
+// Example:
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: [2, 4, 6]
+
+// Solution07:
+
+function findEvenNumbers(numberArray) {
+  let evenNumbersArray = []
+  for (let index = 0; index < numberArray.length; index++) {
+    if (numberArray[index] % 2 === 0) {
+      evenNumbersArray.push(numberArray[index])
+    }
+  }
+  return evenNumbersArray
+}
+
+// console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// Problem 8: Capitalize First Letter of Each Word
+// Write a function that capitalizes the first letter of each word in a string.
+// Example:
+// Input: "hello world"
+// Output: "Hello World"
+
+// Solution08:
+
+function capitalizeFirstLetterOfEachWord(inputString) {
+  let wordsArray = inputString.split(' ')
+  let capitalizedWordsArray = []
+  for (let index = 0; index < wordsArray.length; index++) {
+    let currentWord = wordsArray[index]
+    let capitalizedWord = currentWord[0].toUpperCase() + currentWord.slice(1)
+    capitalizedWordsArray.push(capitalizedWord)
+  }
+  let resultString = capitalizedWordsArray.join(' ')
+  return resultString
+}
+// console.log(capitalizeFirstLetterOfEachWord("hello world"));
+
+// Problem 9: Find the Factorial of a Number
+// Write a function that calculates the factorial of a number using a loop.
+// Example:
+// Input: 5
+// Output: 120
+
+// Solution09:
+function factorialOfNumber(n) {
+  let factorial = 1
+  for (let index = 1; index <= n; index++) {
+    factorial = factorial * index
+  }
+  return factorial
+}
+// console.log(factorialOfNumber(5));
+
+// Problem 10: PingPong Challenge
+// Write a function that prints numbers from 1 to 20.
+// Example:
+// For multiples of 3, print "Ping"
+// For multiples of 5, print "Pong"
+// For multiples of both 3 and 5, print "PingPong"
+// If the number is not a multiple of 3 or 5, print the number itself
+// Example Output:
+// 1, 2, Ping, 4, Pong, Ping, 7, 8, Ping, Pong, 11, Ping, 13, 14, PingPong, 16 …..
+
+// Solution10:
+
+function pingPongChallenge() {
+  for (let number = 1; number <= 20; number++) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log('PingPong')
+    } else if (number % 3 === 0) {
+      console.log('Ping')
+    } else if (number % 5 === 0) {
+      console.log('Pong')
+    } else {
+      console.log(number)
+    }
+  }
+}
+
+console.log(pingPongChallenge())
